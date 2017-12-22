6393f8d580ca6bd678ea667c126d6572397dc916
JaimieWalker <jaimie.walker90@gmail.com>
2017-12-21 20:34:13 -0500

    added a product list, in the product services, next step is to change the product price in real time, which will be done by connecting the product and market service together

--- engine-server/app/controllers/product_controller.rb
+++ engine-server/app/controllers/product_controller.rb
@@ -1,5 +1,6 @@
 class ProductController < ApplicationController
     def get_products
+        # Should a price be added to the product table? Or should we also grab the most recent price match?
         render json: Product.all
     end
 end
