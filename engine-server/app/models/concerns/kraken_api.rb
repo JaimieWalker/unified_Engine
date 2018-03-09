34d603f4a5fce93df9dc5b5ab2f3868d75976a3b
Jaimie <jaimie.walker90@gmail.com>
2018-03-08 19:02:16 -0500

    now just need to create a ticker

--- engine-server/app/models/concerns/kraken_api.rb
+++ engine-server/app/models/concerns/kraken_api.rb
@@ -16,11 +16,30 @@ class KrakenApi
                 temp = String.new(pair[0])
                 temp.insert(3,'-')
                 product = Product.find_by(product_name: temp)
-                
+                update_database(product,pair)
             end
-
         end
     end
 
+    def self.update_database(product_in_db,pair_arr)
+        product_object = pair_arr[1]
+        if product_in_db.nil?
+            product_in_db = Product.create(product_name: product_object["altname"]) do |p|
+                p.base_currency = product_object["base"]
+                 if product_object["quote"].size == 4 && product_object["quote"] != "DASH" && product_object["quote"] != "USDT"
+                     product_object["quote"] = product_object["quote"].slice(1,3)
+                 end   
+                p.quote_currency = product_object["quote"]
+                p.kraken_name = pair_arr[0]
+                
+            end
+        else
+            product_in_db.update(kraken_name: pair_arr[0])
+        end
+    end
+
+    def self.run_kraken_event_loop
+        
+    end
 
 end
