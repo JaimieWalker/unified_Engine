93d770a61dd768115c07fdb0cd592644ddfd6395
Jaimie <jaimie.walker90@gmail.com>
2018-04-18 23:02:03 -0400

    editing recent json

--- engine-server/app/controllers/product_controller.rb
+++ engine-server/app/controllers/product_controller.rb
@@ -11,16 +11,16 @@ class ProductController < ApplicationController
             kraken_match = prod.kraken_matches.last
             coinbase_match = prod.coinbase_matches.last
             gemini_match = prod.gemini_matches.last
-
+# &: is the safe navigation operator. It returns nil if a NoMethodError nil would occur
             product_name = prod.base_currency + '-' + prod.quote_currency
-            products[product_name] => {:gdax_price => gdax_match.price}
-            products[product_name] => {:gdax_time => gdax_match.created_at}
-            products[product_name] => {:kraken_price => kraken_match.price}
-            products[product_name] => {:kraken_time => kraken_match.created_at}
-            products[product_name] => {:coinbase_price => coinbase_match.price}
-            products[product_name] => {:coinbase_time => coinbase_match.created_at}
-            products[product_name] => {:gemini_price => gemini_match.price}
-            products[product_name] => {:gemini_time => gemini_match.created_at}
+            products[product_name] = {:gdax_price => gdax_match&.price}
+            products[product_name] = {:gdax_time => gdax_match&.created_at}
+            products[product_name] = {:kraken_price => kraken_match&.price}
+            products[product_name] = {:kraken_time => kraken_match&.created_at}
+            products[product_name] = {:coinbase_price => coinbase_match&.price}
+            products[product_name] = {:coinbase_time => coinbase_match&.created_at}
+            products[product_name] = {:gemini_price => gemini_match&.price}
+            products[product_name] = {:gemini_time => gemini_match&.created_at}
         end
         render json: products
     end
--- engine-server/app/models/concerns/market_api_connection.rb
+++ engine-server/app/models/concerns/market_api_connection.rb
@@ -12,7 +12,30 @@ module MarketApiConnection
 	@@gemini_ws = []
 	attr_accessor :products
 	
+	def self.recent
+        products = {}
+		Product.all.each do |prod|
+            gdax_match = prod.matches.last
+            kraken_match = prod.kraken_matches.last
+            coinbase_match = prod.coinbase_matches.last
+            gemini_match = prod.gemini_matches.last
+			
+            product_name = prod.base_currency + '-' + prod.quote_currency
+            products[product_name] = {:gdax_price => gdax_match.price}
+            products[product_name] = {:gdax_time => gdax_match.created_at}
+            products[product_name] = {:kraken_price => kraken_match.price}
+            products[product_name] = {:kraken_time => kraken_match.created_at}
+            products[product_name] = {:coinbase_price => coinbase_match.price}
+            products[product_name] = {:coinbase_time => coinbase_match.created_at}
+            products[product_name] = {:gemini_price => gemini_match.price}
+            products[product_name] = {:gemini_time => gemini_match.created_at}
+		end
+		binding.pry
+		
+    end
+	
 	def self.start_api_connection
+		recent
 		binding.pry
 		uri = URI("https://api.gdax.com/products")
 		uri2_gemini = URI("https://api.gemini.com/v1/symbols")
